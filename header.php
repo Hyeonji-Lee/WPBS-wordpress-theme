91e8e8975afbdf12bd443c56ea4d6e1108cec61c
Hyeonji-Lee <hyperlhg@gmail.com>
2017-06-27 16:22:59 +0900

    헤더 디자인 수정

--- header.php
+++ header.php
@@ -26,50 +26,36 @@
 <div class="container">
 	<header id="masthead" class="site-header" role="banner">
 		<div class="row">
-			<div class="col-md-3">
-				<div class="site-branding">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-					<?php
-					endif; ?>
-				</div><!-- .site-branding -->
-			</div><!-- .col-md-3 -->
-		
-		
-			<div class="col-md-9">
-				<nav id="site-navigation" class="navbar navbar-default" role="navigation">
-					<div class="container-fluid">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#wpbs-navbar-collapse-1">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-							<a class="navbar-brand" href="<?php echo home_url(); ?>">
-								<?php bloginfo('name'); ?>
-							</a>
-						</div>
+			<nav id="site-navigation" class="navbar navbar-default" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#wpbs-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!--로고 넣는 자리-->
+						<a class="navbar-brand" href="<?php echo home_url(); ?>">
+							<?php bloginfo('name'); ?>
+						</a>
+					</div>
 
-						<?php
-						wp_nav_menu( array(
-							'manu'				=> 'primary',
-							'theme_location'	=> 'primary',
-							'depth'				=>  2,
-							'container'			=> 'div',
-							'container_class'	=> 'collapse navbar-collapse',
-							'container_id'		=> 'wpbs-navbar-collapse-1',
-							'menu_class'		=> 'nav navbar-nav',
-							'fallback_cb'		=> 'wp_bootstrap_navwalker::fallback',
-							'walker'      		 => new WP_Bootstrap_Navwalker()
-						) );
-					?>
-					</div>	
-				</nav><!-- #site-navigation -->
-			</div><!-- .col-md-9 -->
+					<?php
+					wp_nav_menu( array(
+						'manu'				=> 'primary',
+						'theme_location'	=> 'primary',
+						'depth'				=>  2,
+						'container'			=> 'div',
+						'container_class'	=> 'collapse navbar-collapse',
+						'container_id'		=> 'wpbs-navbar-collapse-1',
+						'menu_class'		=> 'nav navbar-nav',
+						'fallback_cb'		=> 'wp_bootstrap_navwalker::fallback',
+						'walker'      		 => new WP_Bootstrap_Navwalker()
+					) );
+				?>
+				</div>	
+			</nav><!-- #site-navigation -->
 		</div><!-- .row -->
 	</header><!-- #masthead -->
 </div><!-- .container -->
