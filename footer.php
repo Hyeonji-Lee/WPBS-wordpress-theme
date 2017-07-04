20a4f18f093bd6799a7444b910709f21b24e32c8
Hyeonji-Lee <hyperlhg@gmail.com>
2017-07-04 13:01:39 +0900

    푸터 작업 시작

--- footer.php
+++ footer.php
@@ -12,20 +12,21 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpbs' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wpbs' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpbs' ), 'wpbs', '<a href="https://automattic.com/">Fezze</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<div class="container-fluid">
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wpbs' ) ); ?>"><?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'wpbs' ), 'WordPress' );
+				?></a>
+				<span class="sep"> | </span>
+				<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'wpbs' ), 'wpbs', '<a href="https://automattic.com/">Fezze</a>' );
+				?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- .container-fluid -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
