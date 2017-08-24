18c6bfc033de6b00481ce8a20627adfbf5caf97f
Hyeonji-Lee <hyperlhg@gmail.com>
2017-08-24 17:45:27 +0900

    사이드바 위젯에 부트스트랩 컴포넌트 적용

--- functions.php
+++ functions.php
@@ -188,7 +188,7 @@ add_action( 'wp_head', 'webfont_js' );
  * Bootstrap3
  */
 function bootstrap_styles(){
-	wp_enqueue_style( 'bootstrap-stylesheet', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.css' );
+	wp_enqueue_style( 'bootstrap-stylesheet', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap_wpbs_theme.css' );
 	wp_enqueue_script( 'bootstrap-script', get_stylesheet_directory_uri() . '/bootstrap/js/bootstrap.js', array('jquery') );
 	wp_enqueue_style( 'wpbs-stylesheet', get_stylesheet_directory_uri() . '/wpbs-style.css' );
 }
@@ -214,31 +214,8 @@ function wpbs_logo_setup() {
 }
 add_action( 'after_setup_theme', 'wpbs_logo_setup' );
 
-/*검색 폼 부트스트랩*/
-add_filter( 'get_search_form', 'wop_bootstrap_search_form', 100);
-function wop_bootstrap_search_form() {
-    $value_or_placeholder = ( get_search_query() == '' ) ? 'placeholder' : 'value';
-    $label = 'Search';
-    $search_text = 'Search';
-    $button_text = 'Search';
-$form = '<form method="get" class="search-form form-inline" action="'.home_url( '/' ).'" role="search">
-    <div class="form-group">
-        <label class="sr-only sr-only-focusable" for="bsg-search-form">'.esc_html( $label ).'</label>
-        <div class="input-group">
-            <input type="search" class="search-field form-control" id="search" name="s" '.$value_or_placeholder.'="'.esc_attr( $search_text ).'">
-            <span class="input-group-btn">
-                <button type="submit" class="search-submit btn btn-default">
-                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                    <span class="sr-only">'.esc_attr( $button_text ).'</span>
-                </button>
-            </span>
-        </div>
-    </div>
-</form>';
-    return $form;
-}
-
-function my_filter_dynamic_sidebar_params( $sidebar_params ) {
+/*사이드바 위젯에 부트스트랩 컴포넌트 적용*/
+function wpbs_filter_dynamic_sidebar_params( $sidebar_params ) {
 	
 	if ( is_admin() ) {
 		return $sidebar_params;
@@ -248,14 +225,14 @@ function my_filter_dynamic_sidebar_params( $sidebar_params ) {
 	$widget_id = $sidebar_params[0]['widget_id'];
 	
 	$wp_registered_widgets[ $widget_id ]['original_callback'] = $wp_registered_widgets[ $widget_id ]['callback'];
-	$wp_registered_widgets[ $widget_id ]['callback'] = 'my_custom_widget_callback_function';
+	$wp_registered_widgets[ $widget_id ]['callback'] = 'wpbs_custom_widget_callback_function';
 	
 	return $sidebar_params;
 	
 }
-add_filter( 'dynamic_sidebar_params', 'my_filter_dynamic_sidebar_params' );
+add_filter( 'dynamic_sidebar_params', 'wpbs_filter_dynamic_sidebar_params' );
 
-function my_custom_widget_callback_function() {
+function wpbs_custom_widget_callback_function() {
 	
 	global $wp_registered_widgets;
 	$original_callback_params = func_get_args();
@@ -293,7 +270,7 @@ function wpbs_sidewidget_output_filter( $widget_output, $widget_id_base, $widget
 			$widget_output = str_replace('<table id="wp-calendar', '<table class="table table-condensed" id="wp-calendar', $widget_output);
 			break;
 		case 'tag_cloud':
-			$widget_output = str_replace('class="tag-cloud-link', 'class="label label-default tag-cloud-link', $widget_output);
+			$widget_output = str_replace('class="tag-cloud-link', 'class="label label-primary tag-cloud-link', $widget_output);
 			break;
 		case 'archives':
 			$widget_output = str_replace('<ul>', '<ul class="list-group">', $widget_output);
@@ -313,6 +290,9 @@ function wpbs_sidewidget_output_filter( $widget_output, $widget_id_base, $widget
 		case 'recent-comments':
 			$widget_output = str_replace('<ul id="recentcomments">', '<ul id="recentcomments" class="list-group">', $widget_output);
 			$widget_output = str_replace('<li class="recentcomments">', '<li class="recentcomments list-group-item">', $widget_output);
+			$widget_output = str_replace('<span class="comment-author-link">', '<span class="label label-default comment-author-link">', $widget_output);
+			$widget_output = str_replace('(', '', $widget_output);
+			$widget_output = str_replace(')', '', $widget_output);
 			break;
 		case 'pages':
 			$widget_output = str_replace('<ul>', '<ul class="nav nav-stacked nav-pills">', $widget_output);
@@ -320,11 +300,38 @@ function wpbs_sidewidget_output_filter( $widget_output, $widget_id_base, $widget
 		case 'nav_menu':
 			$widget_output = str_replace(' class="menu"', 'class="menu nav nav-stacked nav-pills"', $widget_output);
 			break;
+		case 'grofile':
+			$widget_output = str_replace('<p>', '<p class="label label-primary">', $widget_output);
+			break;
 		
 	}
 	return $widget_output;
 }
 add_filter( 'widget_output', 'wpbs_sidewidget_output_filter', 10, 3 );
+
+/*검색 폼 부트스트랩*/
+add_filter( 'get_search_form', 'wop_bootstrap_search_form', 100);
+function wop_bootstrap_search_form() {
+	$value_or_placeholder = ( get_search_query() == '' ) ? 'placeholder' : 'value';
+	$label = 'Search';
+	$search_text = 'Search';
+	$button_text = 'Search';
+	$form = '<form method="get" class="search-form form-inline" action="'.home_url( '/' ).'" role="search">
+    <div class="form-group">
+        <label class="sr-only sr-only-focusable" for="bsg-search-form">'.esc_html( $label ).'</label>
+        <div class="input-group">
+            <input type="search" class="search-field form-control" id="search" name="s" '.$value_or_placeholder.'="'.esc_attr( $search_text ).'">
+            <span class="input-group-btn">
+                <button type="submit" class="search-submit btn btn-default">
+                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                    <span class="sr-only">'.esc_attr( $button_text ).'</span>
+                </button>
+            </span>
+        </div>
+    </div>
+</form>';
+	return $form;
+}
 
 /*페이지에서 빈 p태그 방지*/
 function remove_empty_p(){
